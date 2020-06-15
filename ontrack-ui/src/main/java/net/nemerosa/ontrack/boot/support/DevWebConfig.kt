072f3fe51e1158405b1786745a559a13f3061bc8
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-06-15 18:45:20 +0200

    [ui] Angular routing support

--- ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/support/DevWebConfig.kt
+++ ontrack-ui/src/main/java/net/nemerosa/ontrack/boot/support/DevWebConfig.kt
@@ -1,18 +1,23 @@
 package net.nemerosa.ontrack.boot.support
 
 import net.nemerosa.ontrack.common.RunProfile
-import org.apache.commons.logging.LogFactory
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.core.io.FileSystemResource
+import org.springframework.core.io.Resource
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.resource.PathResourceResolver
 import java.io.File
 
 @Configuration
 @Profile(RunProfile.DEV)
 class DevWebConfig : WebMvcConfigurer {
-    private val log = LogFactory.getLog(DevWebConfig::class.java)
+
+    private val logger: Logger = LoggerFactory.getLogger(DevWebConfig::class.java)
 
     @Autowired
     private lateinit var devSettings: DevSettings
@@ -25,21 +30,34 @@ class DevWebConfig : WebMvcConfigurer {
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
 
         // Warning
-        log.warn("[dev] Running in DEV mode")
+        logger.warn("[dev] Running in DEV mode")
 
         // Next Gen UI
-        val ng = getNgPath()
-        log.info("[dev] Compiled ng web resources from: $ng")
-        registry.addResourceHandler("/ng/**").addResourceLocations("$ng/")
+
+        val ng = File(devSettings.ngWeb, "dist/ontrack-ui-web")
+        val ngPath = ng.absolutePath
+        logger.info("[dev] Compiled ng web resources from: $ng")
+        val ngIndex = FileSystemResource(File(ng, "index.html"))
+        registry.addResourceHandler("/ng/**")
+                .addResourceLocations("file:$ngPath/")
+                .resourceChain(false) // No caching
+                .addResolver(object : PathResourceResolver() {
+                    override fun getResource(resourcePath: String, location: Resource): Resource? {
+                        return ngIndex
+                    }
+                })
+        registry.addResourceHandler("/ng/*.js").addResourceLocations("file:$ngPath/")
+        registry.addResourceHandler("/ng/assets/*.png").addResourceLocations("file:$ngPath/assets/")
+
 
         // Compiled production resources
         val prod = getPath(devSettings.prod)
-        log.info("[dev] Compiled prod web resources from: $prod")
+        logger.info("[dev] Compiled prod web resources from: $prod")
         registry.addResourceHandler("/fonts/**").addResourceLocations("$prod/fonts/")
 
         // Compiler dev resources
         val dev = getPath(devSettings.dev)
-        log.info("[dev] Compiled dev web resources from: $dev")
+        logger.info("[dev] Compiled dev web resources from: $dev")
         registry.addResourceHandler("/css/**").addResourceLocations("$dev/css/")
         registry.addResourceHandler("/templates/**").addResourceLocations("$dev/templates/")
         registry.addResourceHandler("/converted/**").addResourceLocations("$dev/converted/")
@@ -48,19 +66,16 @@ class DevWebConfig : WebMvcConfigurer {
 
         // Direct access to the sources
         val source = getPath(devSettings.src)
-        log.info("[dev] Web sources from: $source")
+        logger.info("[dev] Web sources from: $source")
         registry.addResourceHandler("/app/**").addResourceLocations("$source/app/")
         registry.addResourceHandler("/graphiql/**").addResourceLocations("$source/graphiql/")
         registry.addResourceHandler("/assets/**").addResourceLocations("$source/assets/")
 
         // Vendor resources
         val vendor = getPath(devSettings.vendor)
-        log.info("[dev] Vendor sources from: $vendor")
+        logger.info("[dev] Vendor sources from: $vendor")
         registry.addResourceHandler("/vendor/**").addResourceLocations("$vendor/")
     }
-
-    private fun getNgPath(): String =
-            "file:" + File(devSettings.ngWeb, "dist/ontrack-ui-web").absolutePath
 
     private fun getPath(dirName: String): String {
         return "file:" + File(
