5021907dbddb64b0716e672eb8179469e5f7ddd6
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2020-06-16 20:36:26 +0200

    [ui] Search box component - UI

--- ontrack-ui-web/src/app/core/search-box/search-box.component.ts
+++ ontrack-ui-web/src/app/core/search-box/search-box.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SearchBoxComponent implements OnInit {
 
   boxId: string;
+  boxClass: string = "form-inline";
 
   constructor() { }
 
